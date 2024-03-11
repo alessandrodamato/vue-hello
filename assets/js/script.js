98e92b5d76235bf7f579f43c1319930f4392fc9c
alessandrodamato <alessandro042004@gmail.com>
2024-03-11 16:40:49 +0100

    [add] bonus clock

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,29 @@ createApp({
     return{
       messaggio: 'Ciao Vue JS!',
       immagine: 'assets/img/polli.jpg',
+      clock: '00:00:00',
     }
   },
+
+  methods:{
+    printClock(){
+      const d = new Date;
+      const h = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
+      const m = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+      const s = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
+      this.clock = `${h}:${m}:${s}`;
+    },
+
+    startClock(){
+      setInterval (() => {
+        this.printClock();
+      }, 1000)
+    },
+  },
+
+  mounted(){
+    this.printClock()
+    this.startClock()
+  }
 
 }).mount('#app');
